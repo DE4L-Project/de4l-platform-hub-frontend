e8ce2d1d1545cd1c3227e67ef1c1d71d89165ece
gaunitz <gaunitz@wifa.uni-leipzig.de>
2020-04-20 10:16:09 +0200

    initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'de4l-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'de4l-start-app';
+}
