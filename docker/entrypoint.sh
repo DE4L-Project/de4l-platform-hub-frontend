8bee0223f78cf1ef605c0ddbf7a0a9d27f81fff7
gaunitz <gaunitz@wifa.uni-leipzig.de>
2022-06-27 19:21:23 +0200

    - fix config file destination

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -3,13 +3,15 @@ echo "Initializing container..."
 
 CONFIG_URL_ESCAPED=$(sed 's/[&/\]/\\&/g' <<<"$CONFIG_URL")
 
+mkdir -p $HTML_DIR/assets/
+
 # Recreate config on each start
-rm -f $HTML_DIR/config.json
-cp /tmp/config.template.json $HTML_DIR/config.json
+rm -f $HTML_DIR/assetsconfig.json
+cp /tmp/config.template.json $HTML_DIR/assets/config.json
 
 echo "Setting config..."
 
-sed -i "s/@@DEFINE_CONFIG_URL@@/$CONFIG_URL_ESCAPED/g" $HTML_DIR/config.json
+sed -i "s/@@DEFINE_CONFIG_URL@@/$CONFIG_URL_ESCAPED/g" $HTML_DIR/assets/config.json
 
 echo "Starting nginx..."
 
